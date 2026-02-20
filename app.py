402d26a2c82df8507bd6a58abdf211a53d120a5b
alvinnjiru <alvinnjiru7@gmail.com>
2026-02-20 10:57:33 +0300

    added info to product details

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import *
 import pymysql
+import os 
 
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = "static/images"
 
 @app.route("/api/signup", methods={"POST"})
 def signup():
@@ -55,6 +57,52 @@ def signin():
         # get user data
         user = cursor.fetchone()
         return jsonify ({"message":"login succesful" , "user": user})
+    
+
+@app.route("/api/add_product", methods = ["POST"])
+# when writing functions u use camel case where 1st word isnt capital but adjacent words are
+def addProduct():
+    # code to execute
+    product_name = request.form["product_name"]
+    product_description = request.form["product_description"]
+    product_category = request.form["product_category"]
+    product_cost  = request.form ["product_cost"]
+    product_image = request.files["product_image"]
+
+    
+
+    print(product_name,product_description,product_category,product_cost,product_image)
+
+    # get image name
+    image_name = product_image.filename
+    print(image_name)
+
+    # save image to images folder
+    file_path = os.path.join(app.config["UPLOAD_FOLDER"], image_name)
+    print(file_path)
+    product_image.save(file_path)
+
+    # create a db connection
+    connection = pymysql.connect(host="localhost", user="root", password="", database="alvin_sokogarden")    
+
+    # create a cursor
+    cursor = connection.cursor()
+
+    # sql to execute
+    sql = "insert into product_details(product_name, product_description, product_category, product_cost, product_image) values (%s, %s, %s, %s, %s)"
+    # data to execute sql query
+    data = (product_name,product_description,product_category,product_cost,image_name)
+
+    # execute the query
+    cursor.execute(sql,data)
+
+    # SAVE THE DATA
+    connection.commit()
+
+    return jsonify({"message":"product added succesfully"})
+
+
+
     
 
 
